1743f18a3ed9f4dce65dab764234f05bc37a9d5b
1261-beep <1261@student.itgymnazium.cz>
2026-09-03 13:49:45 +0200

    Add files via upload

---
+++ ruska_mrdka.py
@@ -0,0 +1,19 @@
+def fib(a):
+    if (a == 1 or a == 2):
+        return 1
+    else:
+        return fib(a-1)+fib(a-2)
+    
+for i in range(30):
+    print(f'{i + 1}. clen Fibonacciho posloupnosti je {fib(i + 1)}.')
+
+print("tungenistantungenistantungtungériaopfíAHrprinttung tugn tugn tung lol kamo ja vubec nevim co mam delat prtože jsem uplnej debil asi kamo ja uz skoro umim psat vsemi deseti kamo di do prdele fakt si velky tlusty hovno co ani nevi co ma delat protoze je zarity jako hovno v prdeli kamo petr matyas je uplnej zmrd a nevi vubec co ma delat kamo tak bud ztivha zticha protoze nema cenu se s tebou bavit")
+
+
+print("tung tung tung utng utngutntugntu tung tung tung tung tung tung tung")
+n = 0
+
+def nigger(n):
+    print("nigger")
+    nigger(n)
+    return nigger(n)
